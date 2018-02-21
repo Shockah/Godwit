66b1a335c0ca10c6033e3478b9b8030e20c436cc
Michał Dolaś <me@shockah.pl>
2018-02-21 16:25:28 +0100

    Asset path argument for LocalOrInternalFileHandleResolver

--- godwit/src/pl/shockah/godwit/LocalOrInternalFileHandleResolver.java
+++ godwit/src/pl/shockah/godwit/LocalOrInternalFileHandleResolver.java
@@ -4,11 +4,27 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.loaders.FileHandleResolver;
 import com.badlogic.gdx.files.FileHandle;
 
+import javax.annotation.Nullable;
+
 public class LocalOrInternalFileHandleResolver implements FileHandleResolver {
+	@Nullable public final String assetDirectory;
+
+	public LocalOrInternalFileHandleResolver() {
+		this(null);
+	}
+
+	public LocalOrInternalFileHandleResolver(@Nullable String assetDirectory) {
+		this.assetDirectory = assetDirectory;
+	}
+
+	private String getLocalFileName(String fileName) {
+		return assetDirectory != null ? String.format("%s/%s", assetDirectory, fileName) : fileName;
+	}
+
 	@Override
 	public FileHandle resolve(String fileName) {
 		if (Gdx.files.isLocalStorageAvailable() && new FileHandle(String.format("%s/%s", Gdx.files.getLocalStoragePath(), fileName)).exists()) {
-			return Gdx.files.local(fileName);
+			return Gdx.files.local(getLocalFileName(fileName));
 		} else {
 			return Gdx.files.internal(fileName);
 		}
