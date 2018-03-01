df1d06363a5a1e16c665d90f3fa11eb79b7c3e61
Michał Dolaś <me@shockah.pl>
2018-03-01 14:10:07 +0100

    MaskEntity

---
+++ godwit/src/pl/shockah/godwit/gl/MaskEntity.java
@@ -0,0 +1,61 @@
+package pl.shockah.godwit.gl;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import pl.shockah.godwit.Entity;
+import pl.shockah.godwit.geom.IVec2;
+import pl.shockah.godwit.geom.ImmutableVec2;
+
+public class MaskEntity extends Entity {
+	@Nullable public Entity mask;
+
+	@Override
+	public void render(@Nonnull Gfx gfx, @Nonnull IVec2 v) {
+		setupMasking(gfx);
+		renderMask(gfx, v + position);
+		gfx.internalEndTick();
+		useMasking(gfx);
+		super.render(gfx, v);
+		gfx.internalEndTick();
+		disableMasking(gfx);
+	}
+
+	protected final void setupMasking(@Nonnull Gfx gfx) {
+		gfx.prepareContext();
+		Gdx.gl.glClearDepthf(1f);
+		Gdx.gl.glClear(GL20.GL_DEPTH_BUFFER_BIT);
+		Gdx.gl.glDepthFunc(GL20.GL_LESS);
+		Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
+		Gdx.gl.glDepthMask(true);
+		Gdx.gl.glColorMask(false, false, false, false);
+	}
+
+	protected final void useMasking(@Nonnull Gfx gfx) {
+		gfx.prepareContext();
+		Gdx.gl.glColorMask(true, true, true, true);
+		Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
+		Gdx.gl.glDepthFunc(GL20.GL_EQUAL);
+	}
+
+	protected final void disableMasking(@Nonnull Gfx gfx) {
+		gfx.prepareContext();
+		Gdx.gl.glDisable(GL20.GL_DEPTH_TEST);
+	}
+
+	public final void renderMask(@Nonnull Gfx gfx) {
+		renderMask(gfx, ImmutableVec2.zero);
+	}
+
+	public final void renderMask(@Nonnull Gfx gfx, float x, float y) {
+		renderMask(gfx, new ImmutableVec2(x, y));
+	}
+
+	public void renderMask(@Nonnull Gfx gfx, @Nonnull IVec2 v) {
+		if (mask != null)
+			mask.render(gfx, v);
+	}
+}
