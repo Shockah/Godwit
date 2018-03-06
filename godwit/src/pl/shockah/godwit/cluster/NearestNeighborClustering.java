66829a2da4f599d28a573b16c1e39de2454c0c35
Michał Dolaś <me@shockah.pl>
2018-03-06 21:39:49 +0100

    Performance improvement

--- godwit/src/pl/shockah/godwit/cluster/NearestNeighborClustering.java
+++ godwit/src/pl/shockah/godwit/cluster/NearestNeighborClustering.java
@@ -2,15 +2,13 @@ package pl.shockah.godwit.cluster;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.Nonnull;
 
-import java8.util.Maps;
+import java8.util.stream.StreamSupport;
 import pl.shockah.func.Func1;
 
 public class NearestNeighborClustering<T> extends Clustering<T> {
@@ -24,14 +22,13 @@ public class NearestNeighborClustering<T> extends Clustering<T> {
 	@SuppressWarnings("unchecked")
 	@Override
 	@Nonnull public List<T>[] getClusters(@Nonnull List<T> vectors, @Nonnull DistanceAlgorithm algorithm) {
-		List<List<T>> clusters = new ArrayList<>();
-		Map<List<T>, Set<T>> distinct = new HashMap<>();
+		List<Cluster> clusters = new ArrayList<>();
 
 		for (T vector : vectors) {
 			float smallestDistance = Float.MAX_VALUE;
-			List<T> closestCluster = null;
-			for (List<T> cluster : clusters) {
-				for (T vector2 : distinct.get(cluster)) {
+			Cluster closestCluster = null;
+			for (Cluster cluster : clusters) {
+				for (T vector2 : cluster.distinct) {
 					float distance = algorithm.getDistance(toVectorFunc.call(vector), toVectorFunc.call(vector2));
 					if (closestCluster == null || distance < smallestDistance) {
 						closestCluster = cluster;
@@ -41,14 +38,20 @@ public class NearestNeighborClustering<T> extends Clustering<T> {
 			}
 
 			if (closestCluster == null || smallestDistance >= threshold) {
-				closestCluster = new ArrayList<>();
+				closestCluster = new Cluster();
 				clusters.add(closestCluster);
 			}
-			closestCluster.add(vector);
-			Maps.computeIfAbsent(distinct, closestCluster, key -> new HashSet<>()).add(vector);
-			distinct.get(closestCluster).add(vector);
+			closestCluster.vectors.add(vector);
+			closestCluster.distinct.add(vector);
 		}
 
-		return clusters.toArray((List<T>[])Array.newInstance(List.class, clusters.size()));
+		return StreamSupport.stream(clusters)
+				.map(cluster -> cluster.vectors)
+				.toArray(size -> (List<T>[])Array.newInstance(List.class, size));
+	}
+
+	protected class Cluster {
+		@Nonnull public final List<T> vectors = new ArrayList<>();
+		@Nonnull public final Set<T> distinct = new HashSet<>();
 	}
 }
