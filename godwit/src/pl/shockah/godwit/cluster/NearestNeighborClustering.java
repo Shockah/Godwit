d9415321eecbcfb1baf027f8e6b9de0e30e5ba04
Michał Dolaś <me@shockah.pl>
2018-03-06 21:19:48 +0100

    Performance improvement

--- godwit/src/pl/shockah/godwit/cluster/NearestNeighborClustering.java
+++ godwit/src/pl/shockah/godwit/cluster/NearestNeighborClustering.java
@@ -2,7 +2,11 @@ package pl.shockah.godwit.cluster;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.annotation.Nonnull;
 
@@ -20,12 +24,13 @@ public class NearestNeighborClustering<T> extends Clustering<T> {
 	@Override
 	@Nonnull public List<T>[] getClusters(@Nonnull List<T> vectors, @Nonnull DistanceAlgorithm algorithm) {
 		List<List<T>> clusters = new ArrayList<>();
+		Map<List<T>, Set<T>> distinct = new HashMap<>();
 
 		for (T vector : vectors) {
 			float smallestDistance = Float.MAX_VALUE;
 			List<T> closestCluster = null;
 			for (List<T> cluster : clusters) {
-				for (T vector2 : cluster) {
+				for (T vector2 : distinct.get(cluster)) {
 					float distance = algorithm.getDistance(toVectorFunc.call(vector), toVectorFunc.call(vector2));
 					if (closestCluster == null || distance < smallestDistance) {
 						closestCluster = cluster;
@@ -37,8 +42,10 @@ public class NearestNeighborClustering<T> extends Clustering<T> {
 			if (closestCluster == null || smallestDistance >= threshold) {
 				closestCluster = new ArrayList<>();
 				clusters.add(closestCluster);
+				distinct.put(closestCluster, new HashSet<>());
 			}
 			closestCluster.add(vector);
+			distinct.get(closestCluster).add(vector);
 		}
 
 		return clusters.toArray((List<T>[])Array.newInstance(List.class, clusters.size()));
