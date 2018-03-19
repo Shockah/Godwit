d3286cb4fcf17435b75aea17255859bcc7245fb6
Michał Dolaś <me@shockah.pl>
2018-03-19 21:55:19 +0100

    Fixed DesktopImagePickerService

--- desktop/src/pl/shockah/godwit/platform/DesktopImagePickerService.java
+++ desktop/src/pl/shockah/godwit/platform/DesktopImagePickerService.java
@@ -2,15 +2,12 @@ package pl.shockah.godwit.platform;
 
 import com.badlogic.gdx.graphics.Pixmap;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.io.File;
 import java.nio.file.Files;
 
 import javax.annotation.Nonnull;
-import javax.swing.JButton;
 import javax.swing.JFileChooser;
-import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -27,27 +24,17 @@ public class DesktopImagePickerService extends ImagePickerService {
 				throw new RuntimeException(e);
 			}
 
-			final JFrame frame = new JFrame("Open image");
-			frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			frame.setLayout(new BorderLayout());
-			JButton openButton = new JButton("Open");
-			openButton.addActionListener(event -> {
-				JFileChooser chooser = new JFileChooser();
-				chooser.setFileFilter(new FileNameExtensionFilter("Images", "jpg", "jpeg", "png", "bmp"));
-				if (chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
-					try {
-						File file = chooser.getSelectedFile();
-						byte[] bytes = Files.readAllBytes(file.toPath());
-						delegate.call(new Pixmap(bytes, 0, bytes.length));
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+			JFileChooser chooser = new JFileChooser();
+			chooser.setFileFilter(new FileNameExtensionFilter("Images", "jpg", "jpeg", "png", "bmp"));
+			if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+				try {
+					File file = chooser.getSelectedFile();
+					byte[] bytes = Files.readAllBytes(file.toPath());
+					delegate.call(new Pixmap(bytes, 0, bytes.length));
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			});
-			frame.add(openButton);
-			frame.pack();
-			frame.setLocationByPlatform(true);
-			frame.setVisible(true);
+			}
 		});
 	}
 }
