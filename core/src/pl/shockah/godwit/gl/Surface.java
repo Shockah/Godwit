e6a23240ccd1048681e06172e67c2acf9d0ab03a
Michał Dolaś <me@shockah.pl>
2018-03-21 09:35:32 +0100

    Surface is now Disposable

--- core/src/pl/shockah/godwit/gl/Surface.java
+++ core/src/pl/shockah/godwit/gl/Surface.java
@@ -3,14 +3,20 @@ package pl.shockah.godwit.gl;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.utils.Disposable;
 
 import javax.annotation.Nonnull;
 
+import lombok.Getter;
+import pl.shockah.godwit.Godwit;
 import pl.shockah.godwit.geom.IVec2;
 
-public class Surface extends GfxImpl implements Renderable {
+public class Surface extends GfxImpl implements Renderable, Disposable {
 	@Nonnull public final FrameBuffer fbo;
 	@Nonnull public final TextureRegion region;
+
+	@Getter
+	private boolean disposed = false;
 
 	@Nonnull public static Surface create(int width, int height) {
 		return create(width, height, false);
@@ -24,27 +30,43 @@ public class Surface extends GfxImpl implements Renderable {
 	protected Surface(int width, int height, boolean depth) {
 		fbo = new FrameBuffer(Pixmap.Format.RGBA8888, width, height, depth, false);
 		region = new TextureRegion(fbo.getColorBufferTexture(), 0, 0, width, height);
-		region.flip(false, true);
+		region.flip(false, Godwit.getInstance().yPointingDown);
 	}
 
 	@Override
 	public int getWidth() {
+		if (disposed)
+			throw new IllegalStateException("Surface is disposed.");
 		return fbo.getWidth();
 	}
 
 	@Override
 	public int getHeight() {
+		if (disposed)
+			throw new IllegalStateException("Surface is disposed.");
 		return fbo.getHeight();
 	}
 
 	@Override
 	protected void prepareContext() {
+		if (disposed)
+			throw new IllegalStateException("Surface is disposed.");
 		GfxContextManager.bindSurface(this);
 	}
 
 	@Override
 	public void render(@Nonnull Gfx gfx, @Nonnull IVec2 v) {
+		if (disposed)
+			throw new IllegalStateException("Surface is disposed.");
 		gfx.prepareSprites();
 		gfx.getSpriteBatch().draw(region, v.x(), v.y());
+	}
+
+	@Override
+	public void dispose() {
+		if (disposed)
+			return;
+		disposed = true;
+		fbo.dispose();
 	}
 }
