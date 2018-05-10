72b880e42b605d434fcf413e8cee4322288bcfa9
Michał Dolaś <me@shockah.pl>
2018-05-10 15:01:56 +0200

    WIP: iOS scale fix

--- ios/src/pl/shockah/godwit/platform/IosSafeAreaService.java
+++ ios/src/pl/shockah/godwit/platform/IosSafeAreaService.java
@@ -3,6 +3,7 @@ package pl.shockah.godwit.platform;
 import org.robovm.apple.uikit.UIApplication;
 import org.robovm.apple.uikit.UIDevice;
 import org.robovm.apple.uikit.UIEdgeInsets;
+import org.robovm.apple.uikit.UIScreen;
 import org.robovm.apple.uikit.UIWindow;
 
 import javax.annotation.Nonnull;
@@ -20,7 +21,14 @@ public class IosSafeAreaService extends SafeAreaService {
 		if (version.length >= 1 && version[0] >= 11) {
 			UIWindow window = UIApplication.getSharedApplication().getKeyWindow();
 			UIEdgeInsets insets = window.getSafeAreaInsets();
-			return new Padding((float)insets.getLeft(), (float)insets.getRight(), (float)insets.getTop(), (float)insets.getBottom());
+
+			float scale = (float)UIScreen.getMainScreen().getScale();
+			return new Padding(
+					(float)insets.getLeft() * scale,
+					(float)insets.getRight() * scale,
+					(float)insets.getTop() * scale,
+					(float)insets.getBottom() * scale
+			);
 		} else {
 			return super.getSafeAreaPadding();
 		}
