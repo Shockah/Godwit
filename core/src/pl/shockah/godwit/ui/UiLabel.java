d5bbefeea5ac71eece83289a298d2445d38cb00f
Michał Dolaś <me@shockah.pl>
2018-06-19 12:38:24 +0200

    UiLabel

---
+++ core/src/pl/shockah/godwit/ui/UiLabel.java
@@ -0,0 +1,38 @@
+package pl.shockah.godwit.ui;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import pl.shockah.godwit.geom.IVec2;
+import pl.shockah.godwit.geom.Rectangle;
+import pl.shockah.godwit.gl.Gfx;
+import pl.shockah.godwit.gl.GfxFont;
+
+public class UiLabel extends UiControl<Rectangle> {
+	@Nonnull
+	public GfxFont font;
+
+	@Nullable
+	public String text;
+
+	public UiLabel(@Nonnull GfxFont font) {
+		this.font = font;
+	}
+
+	@Nullable
+	@Override
+	public Rectangle getGestureShape() {
+		return getBounds();
+	}
+
+	@Override
+	public void render(@Nonnull Gfx gfx, @Nonnull IVec2 v) {
+		if (text != null && !text.equals("")) {
+			gfx.getScissors().push(getBounds(), getCameraGroup().getCamera());
+			font.setText(text);
+			font.setMaxWidth(size.x);
+			font.render(gfx, position.add(size.multiply(font.getAlignment().getVector())));
+			gfx.getScissors().pop();
+		}
+	}
+}
--- test/src/pl/shockah/godwit/test/Ui1Test.java
+++ test/src/pl/shockah/godwit/test/Ui1Test.java
@@ -1,6 +1,7 @@
 package pl.shockah.godwit.test;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +9,8 @@ import java.util.List;
 import javax.annotation.Nonnull;
 
 import pl.shockah.godwit.State;
+import pl.shockah.godwit.asset.FreeTypeFontAsset;
+import pl.shockah.godwit.asset.FreeTypeFontLoader;
 import pl.shockah.godwit.constraint.AxisConstraint;
 import pl.shockah.godwit.constraint.BasicConstraint;
 import pl.shockah.godwit.constraint.CenterConstraint;
@@ -17,19 +20,37 @@ import pl.shockah.godwit.constraint.Constraint;
 import pl.shockah.godwit.constraint.FitChildrenConstraint;
 import pl.shockah.godwit.geom.IVec2;
 import pl.shockah.godwit.gl.Gfx;
+import pl.shockah.godwit.gl.GfxFont;
+import pl.shockah.godwit.ui.Alignment;
 import pl.shockah.godwit.ui.UiButton;
+import pl.shockah.godwit.ui.UiLabel;
 import pl.shockah.godwit.ui.UiScroll;
 import pl.shockah.godwit.ui.Unit;
 
 public class Ui1Test extends State {
 	public Ui1Test() {
+		FreeTypeFontAsset fontAsset = new FreeTypeFontAsset("Jellee-Roman.ttf", new FreeTypeFontLoader.FreeTypeFontParameter() {{
+			size = 24;
+			minFilter = magFilter = Texture.TextureFilter.Linear;
+		}});
+		loadAsset(fontAsset);
+
+		UiLabel label;
+		ui.addChild(label = new UiLabel(new GfxFont(fontAsset)));
+		label.addConstraint(new BasicConstraint(label, Constraint.Attribute.Width, safeArea));
+		label.addConstraint(new BasicConstraint(label.getAttributes().height, new Unit.Pixels(96f)));
+		label.addConstraint(new CenterConstraint(label, safeArea, AxisConstraint.Axis.Horizontal));
+		label.addConstraint(new BasicConstraint(label, Constraint.Attribute.Top, safeArea));
+		label.font.setAlignment(Alignment.Horizontal.Center.and(Alignment.Vertical.Middle));
+		label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
 		UiScroll scroll;
 		ui.addChild(scroll = new UiScroll(UiScroll.Direction.Vertical));
 
 		scroll.addConstraint(new BasicConstraint(scroll, Constraint.Attribute.Width, safeArea, new Unit.Pixels(-32f)));
-		scroll.addConstraint(new BasicConstraint(scroll, Constraint.Attribute.Height, safeArea, new Unit.Pixels(-32f)));
+		scroll.addConstraint(new BasicConstraint(scroll, Constraint.Attribute.Height, safeArea, new Unit.Pixels(-128f)));
 		scroll.addConstraint(new CenterConstraint(scroll, safeArea, AxisConstraint.Axis.Horizontal));
-		scroll.addConstraint(new CenterConstraint(scroll, safeArea, AxisConstraint.Axis.Vertical));
+		scroll.addConstraint(new BasicConstraint(scroll, Constraint.Attribute.Bottom, safeArea));
 
 		List<UiButton> buttons = new ArrayList<>();
 		for (int i = 0; i < 50; i++) {
